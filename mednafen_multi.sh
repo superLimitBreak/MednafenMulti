923d5d270571525db5f6a777ace520e9bd533bdc
Richard Lancaster <lancasterrich@gmail.com>
2017-01-29 16:41:21 +0000

    Support recording a single video/multiple videos

--- mednafen_multi.sh
+++ mednafen_multi.sh
@@ -6,7 +6,7 @@ MEDNAFEN_SERVER=${MEDNAFEN_SERVER:-~/src/mednafen-server/run.sh}
 
 
 usage() {
-    printf "Usage: mednafen_multi.sh game [-c|--count window_count] [-r|--record] [-d|--record-dir]\n"
+    printf "Usage: mednafen_multi.sh game [-c|--count window_count] [-r|--record] [-d|--record-dir] [-s|--record-single]\n"
 }
 
 
@@ -33,6 +33,10 @@ while [[ $# -gt 0 ]]; do
         RECORD_DIR=$2
         shift 2
         ;;
+    -s|--record-single)
+        RECORD_SINGLE=true
+        shift
+        ;;
     *)
         usage
         printf "Unknown argument: $1\n"
@@ -43,10 +47,19 @@ done
 
 WINDOW_COUNT=${WINDOW_COUNT:-0}
 RECORD=${RECORD:-false}
+if [[ WINDOW_COUNT -lt 1 ]]; then
+    RECORD=false
+fi
 if $RECORD; then
     BASE_NAME=$(basename "$GAME")
     RECORD_DIR=${RECORD_DIR:-"/tmp/mednafen/$BASE_NAME/"}
-    RECORD_NAME="${BASE_NAME}_$(date -Iseconds).mp4"
+    RECORD_NAME="${BASE_NAME}_$(date -Iseconds)_%s.mp4"
+    RECORD_SINGLE=${RECORD_SINGLE:-false}
+    if $RECORD_SINGLE; then
+        RECORD_FUNC="record_single"
+    else
+        RECORD_FUNC="record"
+    fi
     mkdir -p "$RECORD_DIR"
 fi
 
@@ -60,6 +73,40 @@ on_exit() {
 }; trap "on_exit" EXIT
 
 
+get_ffmpeg_input() {
+    printf -- "-f x11grab -r 60 -video_size 1280x800 -i :$1"
+}
+
+
+get_ffmpeg_output() {
+    printf -- "-c:v libx264 -preset ultrafast -crf 22"
+}
+
+
+record_single() {
+    input_str=""
+    filter_str="[0:v]pad=iw*${WINDOW_COUNT}:ih[t0]"
+    map_str=""
+    for window in $(seq 1 $WINDOW_COUNT); do
+        input_str="${input_str} $(get_ffmpeg_input ${window})"
+        if [[ window -lt WINDOW_COUNT ]]; then
+            filter_str="${filter_str}; [t$(( $window-1 ))][$window:v]overlay=w:x=$(( 1280*(window) ))[t$window]"
+            map_str="[t$window]"
+        fi
+    done
+    ffmpeg ${input_str} -filter_complex "${filter_str}" -map "$map_str"\
+        $(get_ffmpeg_output single)  "$(printf -- "$RECORD_DIR/$RECORD_NAME" single)" &
+}
+
+
+record() {
+    for window in $(seq 1 $WINDOW_COUNT); do
+        ffmpeg $(get_ffmpeg_input $window) $(get_ffmpeg_output $window) \
+            "$(printf -- "$RECORD_DIR/$RECORD_NAME" $window)" &
+    done
+}
+
+
 main() {
     $MEDNAFEN_SERVER & sleep 1
     if [[ WINDOW_COUNT -gt 0 ]]; then
@@ -70,22 +117,10 @@ main() {
             ) &
         done
     fi
-    sleep 5;
+    sleep $(( 3*WINDOW_COUNT ))
 
-    if $RECORD && [[ WINDOW_COUNT -gt 0 ]] ; then
-        input_str=""
-        filter_str="[0:v]pad=iw*${WINDOW_COUNT}:ih[t0]"
-        map_str=""
-        for window in $(seq 1 $WINDOW_COUNT); do
-            input_str="${input_str} -f x11grab -r 60 -video_size 1280x800 -i :${window}"
-            if [[ window -lt WINDOW_COUNT ]]; then
-                filter_str="${filter_str}; [t$(( $window-1 ))][$window:v]overlay=w:x=$(( 1280*(window) ))[t$window]"
-                map_str="[t$window]"
-            fi
-        done
-        ffmpeg ${input_str} -filter_complex "${filter_str}" -map "$map_str"\
-            -c:v libx264 -preset ultrafast -crf 22 \
-            "$RECORD_DIR/$RECORD_NAME" &
+    if $RECORD; then
+        $RECORD_FUNC
     fi
 
     $MEDNAFEN -netplay.nick "master" -connect "$GAME" &
